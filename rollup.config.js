6f9fe6010b5c77608f8c1c189dba171f46018087
Stephane Vanraes <stephane.vanraes@gmail.com>
2020-01-11 21:24:33 +0100

    Fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,20 @@
-import svelte from 'rollup-plugin-svelte';	import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';	import resolve from 'rollup-plugin-node-resolve';
-import pkg from './package.json';	import { terser } from 'rollup-plugin-terser';
+import svelte from 'rollup-plugin-svelte';
+import resolve from 'rollup-plugin-node-resolve';
+import pkg from './package.json';
 
 const name = pkg.name
 	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
 	.replace(/^\w/, m => m.toUpperCase())
 	.replace(/-\w/g, m => m[1].toUpperCase());
 
-
-export default {	export default {
-	input: 'src/index.svelte',	    input: './src/pages/index.js',
-	output: [	    output: {
-		{ file: pkg.module, 'format': 'es' },	        file: './index.js',
-		{ file: pkg.main, 'format': 'umd', name }	        format: 'iife'
-	],	    },
-	plugins: [	    treeshake: true,
-		svelte(),	    plugins: [
-		resolve()	        resolve({
-	]	            extensions: ['.js', '.mjs', '.svelte']
+export default {
+	input: 'src/index.svelte',
+	output: [
+		{ file: pkg.module, 'format': 'es' },
+		{ file: pkg.main, 'format': 'umd', name }
+	],
+	plugins: [
+		svelte(),
+		resolve()
+	]
 };
