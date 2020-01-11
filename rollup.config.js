0281de08c2ffb3345944d3dda8f78888d2e887ad
Stephane Vanraes <stephane.vanraes@gmail.com>
2020-01-11 21:22:33 +0100

    Fix changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,21 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+import svelte from 'rollup-plugin-svelte';	import svelte from 'rollup-plugin-svelte';
+import resolve from 'rollup-plugin-node-resolve';	import resolve from 'rollup-plugin-node-resolve';
+import pkg from './package.json';	import { terser } from 'rollup-plugin-terser';
 
-export default {
-    input: './src/pages/index.js',
-    output: {
-        file: './index.js',
-        format: 'iife'
-    },
-    treeshake: true,
-    plugins: [
-        resolve({
-            extensions: ['.js', '.mjs', '.svelte']
-        }),
-        svelte(),
-        terser()
-    ],
-}
+const name = pkg.name
+	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
+	.replace(/^\w/, m => m.toUpperCase())
+	.replace(/-\w/g, m => m[1].toUpperCase());
+
+
+export default {	export default {
+	input: 'src/index.svelte',	    input: './src/pages/index.js',
+	output: [	    output: {
+		{ file: pkg.module, 'format': 'es' },	        file: './index.js',
+		{ file: pkg.main, 'format': 'umd', name }	        format: 'iife'
+	],	    },
+	plugins: [	    treeshake: true,
+		svelte(),	    plugins: [
+		resolve()	        resolve({
+	]	            extensions: ['.js', '.mjs', '.svelte']
+};
