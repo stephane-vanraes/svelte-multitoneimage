a4eab4d8c070f3e3a96de329ad49533e684821aa
Stephane Vanraes <stephane.vanraes@gmail.com>
2020-02-04 23:19:11 +0100

    Make id reactive

--- src/index.svelte
+++ src/index.svelte
@@ -6,7 +6,7 @@
     export let exponent = 1;
 
     // Creates a rough hash to ensure unique id's
-    let id = [src, ...colours]
+    $: id = [src, ...colours]
         .join()
         .split('')
         .reduce((a,b) => (((a << 5) - a) + b.charCodeAt(0))|0, 0);
